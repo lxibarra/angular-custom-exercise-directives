122774835585715b1d982d4b4b900bf716a19c86
Ricardo Ibarra <ribarra@tekmexico.com>
2015-08-25 15:16:02 -0700

    everything has been moved to a directive

--- app/scripts/controllers/endlesscroll.js
+++ app/scripts/controllers/endlesscroll.js
@@ -8,43 +8,7 @@
  * Controller of the angularExamApp
  */
 angular.module('angularExamApp')
-  .controller('EndlesscrollCtrl', function (imageProvider, $scope, $window, $document) {
+  .controller('EndlesscrollCtrl', function () {
 
-      var windowEl = angular.element($window);
-      var documentEl = angular.element($document);
-      var page = 1;
-
-      $scope.data = [];
-      //we pre inject service dependencies
-      imageProvider.set(['imgur']);
-
-      angular.element('#indicator').show();
-      var promise = imageProvider.get('imgur', 'gallery/hot/viral', undefined, page, 9);
-      promise.then(function(data) {
-          $scope.$apply(function() {
-            $scope.data = data;
-            angular.element('#indicator').hide();
-          });
-      });
-
-      windowEl.on('scroll', function() {
-
-          if(windowEl.scrollTop() == (documentEl.height() - windowEl.height())) {
-            if(page > 16) {
-              page = 1;
-            } else {
-              page++;
-            }
-
-            angular.element('#indicator').show();
-            var promise = imageProvider.get('imgur', 'gallery/hot/viral', undefined, page, 9);
-            promise.then(function(data) {
-              $scope.$apply(function() {
-                $scope.data = data;
-                angular.element('#indicator').hide();
-              });
-            });
-          }
-      });
 
   });
--- app/scripts/directives/infinitescroll.js
+++ app/scripts/directives/infinitescroll.js
@@ -7,37 +7,61 @@
  * # infiniteScroll
  */
 angular.module('angularExamApp')
-  .directive('infiniteScroll', function () {
+  .directive('infiniteScroll', function (imageProvider, $window, $document) {
     return {
       restrict: 'E',
-      template:'<div></div>',
-      replace:true,
-      scope:{
-        images:'=images'
-      },
+      template: '<div></div>',
+      replace: true,
       link: function postLink(scope, element, attrs) {
-        element.on('mousemove', function(evt) {
-          if(evt.target.className === 'scroller-thumbnail') {
+        var windowEl = angular.element($window);
+        var documentEl = angular.element($document);
+        var imageSources = attrs.imageSource.split(',');
+        var page = 1;
+        imageProvider.set(imageSources);
+
+
+        function getImages(source, channel, queryString, page, limit) {
+          var promise = imageProvider.get(source, channel, queryString, page, limit);
+          promise.then(function (data) {
+            data.forEach(function (img) {
+              var col = angular.element('<div/>');
+              col.addClass('col-xs-6 col-md-4 mario');
+              var item = angular.element('<div/>');
+              item.attr('data-url', "url('" + img.url + "')");
+              item.addClass('scroller-thumbnail');
+              item.attr('style', "background: url('" + img.url + "') center no-repeat;");
+              var image = angular.element('<img/>');
+              image.attr('src', img.url);
+              item.append(image);
+              col.append(item);
+              element.append(col);
+            });
+            angular.element(attrs.indicator).hide();
+          });
+        }
+
+        angular.element(attrs.indicator).show();
+        getImages(imageSources[0], attrs.channel, undefined, page, 9); //nine should be a limit set from outside
+
+        element.on('mousemove', function (evt) {
+          if (evt.target.className === 'scroller-thumbnail') {
             var percentageX = Math.round((evt.offsetX / evt.target.clientWidth) * 100);
-            var percentageY =  Math.round((evt.offsetY / evt.target.clientHeight) * 100);
+            var percentageY = Math.round((evt.offsetY / evt.target.clientHeight) * 100);
             evt.target.style.backgroundPosition = percentageX + "% " + percentageY + "%";
           }
         });
 
-        element.on('mouseout', function(evt) {
+        element.on('mouseout', function (evt) {
           if (evt.target.className === 'scroller-thumbnail') {
             evt.target.style.backgroundPosition = "50% 50%";
           }
         });
 
-        element.on('click', function(evt) {
-
-
-          //console.log(evt.target.attributes['data-url'].nodeValue);
+        element.on('click', function (evt) {
 
           if (evt.target.className === 'scroller-thumbnail') {
 
-            if(evt.target.parentElement.className.toString().match(/full/)) {
+            if (evt.target.parentElement.className.toString().match(/full/)) {
               evt.target.parentElement.className = evt.target.parentElement.className.toString().replace(/full/, '');
               evt.target.style.backgroundImage = evt.target.attributes['data-url'].nodeValue;
             } else {
@@ -50,20 +74,19 @@ angular.module('angularExamApp')
           }
         });
 
-        scope.$watch('images', function() {
-          scope.images.forEach(function(img) {
-            var col = angular.element('<div/>');
-            col.addClass('col-xs-6 col-md-4 mario');
-            var item = angular.element('<div/>');
-            item.attr('data-url', "url('" + img.url + "')");
-            item.addClass('scroller-thumbnail');
-            item.attr('style', "background: url('" + img.url + "') center no-repeat;");
-            var image = angular.element('<img/>');
-            image.attr('src', img.url);
-            item.append(image);
-            col.append(item);
-            element.append(col);
-          });
+        windowEl.on('scroll', function() {
+
+          if(windowEl.scrollTop() == (documentEl.height() - windowEl.height())) {
+            if(page > 16) {
+              page = 1;
+            } else {
+              page++;
+            }
+
+            angular.element(attrs.indicator).show();
+            getImages(imageSources[0], attrs.channel, undefined, page, 9);
+
+          }
         });
       }
     };
